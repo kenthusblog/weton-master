6349b720bc53ff7e017b7b0a5b558d61cd7358ec
citogok <47094667+kenthusblog@users.noreply.github.com>
2022-04-02 10:12:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,15 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@491&display=swap" rel="stylesheet">
+    
+        <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+   
+    
     <link rel="stylesheet" href="style.css" type="text/css"> 
 </head>
 <body>
@@ -48,7 +57,9 @@
                     </div>
                 </div>
                 <input type="button" value="Hitung" id="submit" class="btn btn-primary mt-2 col-12 btn-hitung">
-                <div id="emailHelp" class="form-text"><br><b>Disclaimer!</b> Cuma iseng buat, tidak bisa menjadi patokan dalam kehidupan. Semuanya kembali ke kepercayaan masing-masing.</div>
+                <div class="alert alert-info alert-dismissible fade show" role="alert">
+               <br><b>Disclaimer!</b> Pemahaman ini tidak bisa dijadikan patokan utama dalam kehidupan sehari-hari. 
+              Tergantung dari sisi kepercayaan masing-masing. </div>
             </form>
         </div>
     </div>
